ba77c42ff60cf4c0dce104780285aff58b6a2074
abdullahalshawafi <abdullahadel.aam@gmail.com>
2022-03-20 19:51:25 +0200

    Fixed some spelling errors in main.cpp and let the formatter do his thing in the states files (No real changes in the content)

--- CMP3060 Phase 1 - Student Version/source/states/clear_color_state.hpp
+++ CMP3060 Phase 1 - Student Version/source/states/clear_color_state.hpp
@@ -2,31 +2,34 @@
 
 #include <application.hpp>
 
-class ClearColorState: public our::State {
-    private:
-    double r = 0.0;
-    double g = 0.0;
-    double b = 0.0;
-    double a = 0.0;
-    
+class ClearColorState : public our::State
+{
+private:
+    GLfloat r = 0.0;
+    GLfloat g = 0.0;
+    GLfloat b = 0.0;
+    GLfloat a = 0.0;
+
     // onInitialize() function is called once before the state starts
-    void onInitialize() override {
-        //TODO: Read the color from the configuration file and use it to set the clear color for the window
-        //HINT: you can the configuration for the whole application using "getApp()->getConfig()"
-        //To see how the clear color is written in the json files, see "config/blue-screen.json"
-        //To know how read data from a nlohmann::json object, 
-        //look at the following documentation: https://json.nlohmann.me/features/element_access/
-        const nlohmann::json& clearColor = getApp()->getConfig().at("scene").at("clear-color");
+    void onInitialize() override
+    {
+        // TODO: Read the color from the configuration file and use it to set the clear color for the window
+        // HINT: you can the configuration for the whole application using "getApp()->getConfig()"
+        // To see how the clear color is written in the json files, see "config/blue-screen.json"
+        // To know how read data from a nlohmann::json object,
+        // look at the following documentation: https://json.nlohmann.me/features/element_access/
+        const nlohmann::json &clearColor = getApp()->getConfig().at("scene").at("clear-color");
         r = clearColor.at("r");
         g = clearColor.at("g");
         b = clearColor.at("b");
         a = clearColor.at("a");
     }
 
-    // onDraw(deltaTime) function is called every frame 
-    void onDraw(double deltaTime) override {
-        //At the start of frame we want to clear the screen. Otherwise we would still see the results from the previous frame.
-        glClearColor(r , g , b , a);
+    // onDraw(deltaTime) function is called every frame
+    void onDraw(double deltaTime) override
+    {
+        // At the start of frame we want to clear the screen. Otherwise we would still see the results from the previous frame.
+        glClearColor(r, g, b, a);
         glClear(GL_COLOR_BUFFER_BIT);
     }
 };
--- CMP3060 Phase 1 - Student Version/source/states/fullscreen_triangle_state.hpp
+++ CMP3060 Phase 1 - Student Version/source/states/fullscreen_triangle_state.hpp
@@ -3,26 +3,30 @@
 #include <application.hpp>
 #include <shader/shader.hpp>
 
-// This function allows us to read glm vectors from json 
-namespace glm {
-    template<length_t L, typename T, qualifier Q>
-    void from_json(const nlohmann::json& j, vec<L, T, Q>& v){
-        for(length_t index = 0; index < L; ++index)
+// This function allows us to read glm vectors from json
+namespace glm
+{
+    template <length_t L, typename T, qualifier Q>
+    void from_json(const nlohmann::json &j, vec<L, T, Q> &v)
+    {
+        for (length_t index = 0; index < L; ++index)
             v[index] = j[index].get<T>();
     }
 }
 
-class FullscreenTriangleState: public our::State {
-    
+class FullscreenTriangleState : public our::State
+{
+
     our::ShaderProgram program;
-    //TODO: Add a variable in which we will store the name (ID) for a vertex array
+    // TODO: Add a variable in which we will store the name (ID) for a vertex array
     GLuint Vertex_ArrID;
 
     // onInitialize() function is called once before the state starts
-    void onInitialize() override {
-        
+    void onInitialize() override
+    {
+
         // Here we get the json of the scene configuration
-        const auto& config = getApp()->getConfig()["scene"];
+        const auto &config = getApp()->getConfig()["scene"];
 
         // Then we get the path for vertex and fragment shader
         // We used "assets/shaders/fullscreen.vert" and "assets/shaders/circle.frag" as the defaults if the paths are not defined
@@ -38,44 +42,52 @@ class FullscreenTriangleState: public our::State {
         // We call use() since we will send uniforms to this program
         program.use();
         // We loop over every uniform in the configuration and send to the program
-        if(const auto& uniforms = config["uniforms"]; uniforms.is_object()){
-            for(auto& [name, uniform] : uniforms.items()){
+        if (const auto &uniforms = config["uniforms"]; uniforms.is_object())
+        {
+            for (auto &[name, uniform] : uniforms.items())
+            {
                 std::string type = uniform.value("type", "");
-                if(type == "float"){
+                if (type == "float")
+                {
                     float value = uniform.value("value", 0.0f);
                     program.set(name, value);
-                } else if(type == "vec2"){
-                    glm::vec2 value = uniform.value("value", glm::vec2(0,0));
+                }
+                else if (type == "vec2")
+                {
+                    glm::vec2 value = uniform.value("value", glm::vec2(0, 0));
                     program.set(name, value);
-                } else if(type == "vec4"){
-                    glm::vec4 value = uniform.value("value", glm::vec4(0,0,0,0));
+                }
+                else if (type == "vec4")
+                {
+                    glm::vec4 value = uniform.value("value", glm::vec4(0, 0, 0, 0));
                     program.set(name, value);
                 }
             }
         }
 
-        //TODO: Create a vertex Array
+        // TODO: Create a vertex Array
         glGenVertexArrays(1, &Vertex_ArrID);
 
         // We set the clear color to be black
         glClearColor(0.0, 0.0, 0.0, 1.0);
     }
 
-    // onDraw(deltaTime) function is called every frame 
-    void onDraw(double deltaTime) override {
-        //At the start of frame we want to clear the screen. Otherwise we would still see the results from the previous frame.
+    // onDraw(deltaTime) function is called every frame
+    void onDraw(double deltaTime) override
+    {
+        // At the start of frame we want to clear the screen. Otherwise we would still see the results from the previous frame.
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //TODO: Draw a triangle using the vertex array and the program
-        glBindVertexArray(Vertex_ArrID); 
+        // TODO: Draw a triangle using the vertex array and the program
+        glBindVertexArray(Vertex_ArrID);
         glDrawArrays(GL_TRIANGLES, 0, 3);
-
     }
 
     // onInitialize() function is called once after the state ends
-    void onDestroy() override {
-        //TODO: Delete the vertex Array
-        // first-param : specify the number of vertex array object to be deleted.
-        glDeleteVertexArrays(1,&Vertex_ArrID);
+    void onDestroy() override
+    {
+        // TODO: Delete the vertex Array
+        //  first-param : specify the number of vertex array object to be deleted.
+        glDeleteVertexArrays(1, &Vertex_ArrID);
     }
 };
