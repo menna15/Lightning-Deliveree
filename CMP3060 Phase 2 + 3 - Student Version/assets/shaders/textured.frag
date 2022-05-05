a877f233f433cf8b6a7a8e6422bdee1678344c98
nada elsayed <49396399+nadaelsayed11@users.noreply.github.com>
2022-05-06 01:37:38 +0200

    req 6
    it isn't working yet :disappointed_relieved:

--- CMP3060 Phase 2 + 3 - Student Version/assets/shaders/textured.frag
+++ CMP3060 Phase 2 + 3 - Student Version/assets/shaders/textured.frag
@@ -9,9 +9,11 @@ out vec4 frag_color;
 
 uniform vec4 tint;
 uniform sampler2D tex;
+uniform float alphaThreshold;
 
 void main(){
     //TODO: (Req 6) Modify the following line to compute the fragment color
     // by multiplying the tint with the vertex color and with the texture color 
-    frag_color = vec4(0.0, 0.0, 0.0, 1.0);
+    frag_color = tint * fs_in.color * texture(tex, fs_in.tex_coord);
+    if(frag_color.a < alphaThreshold) discard;
 }
--- CMP3060 Phase 2 + 3 - Student Version/source/common/material/material.cpp
+++ CMP3060 Phase 2 + 3 - Student Version/source/common/material/material.cpp
@@ -8,6 +8,8 @@ namespace our {
     // This function should setup the pipeline state and set the shader to be used
     void Material::setup() const {
         //TODO: (Req 6) Write this function
+        pipelineState.setup();
+        shader->use();
     }
 
     // This function read the material data from a json object
@@ -25,6 +27,8 @@ namespace our {
     // set the "tint" uniform to the value in the member variable tint 
     void TintedMaterial::setup() const {
         //TODO: (Req 6) Write this function
+        Material::setup();
+        shader->set("tint", tint);
     }
 
     // This function read the material data from a json object
@@ -39,6 +43,12 @@ namespace our {
     // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex" 
     void TexturedMaterial::setup() const {
         //TODO: (Req 6) Write this function
+        TintedMaterial::setup();
+        shader->set("alphaThreshold", alphaThreshold);
+        glActiveTexture(GL_TEXTURE0);
+        texture->bind();
+        sampler->bind(0);
+        shader->set("tex", 0);
     }
 
     // This function read the material data from a json object
